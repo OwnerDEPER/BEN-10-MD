93565d6db3badd318483fd18eacc8c060a60598e
OwnerDEPER <syssolutions.pekerele@gmail.com>
2025-07-27 20:15:13 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: '',
+  packname: 'SysSolutions™',
   author: '‎',
   botName: ".",
   botOwner: '103', // Your name
-  ownerNumber: '263780145644', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '51941847465', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
